80079027f7ae5ec9be8461ea18dbc2c189d543fe
delfme <53510751+delfme@users.noreply.github.com>
2023-02-13 10:33:09 +0100

    Add files via upload

---
+++ demo_page.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DemoPage extends StatelessWidget {
+  const DemoPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cube Transition'),
+      ),
+      body: Center(
+        
+      ),
+    );
+  }
+}
---
+++ demo_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'package:cube_transition/cube_transition.dart';
+import 'package:flutter_native_text_input_example/demo_page.dart';
+import 'package:flutter_native_label/flutter_native_label.dart';
+
+class DemoScreen extends StatefulWidget {
+  const DemoScreen({Key? key}) : super(key: key);
+  @override
+  State<DemoScreen> createState() => _DemoScreenState();
+}
+
+class _DemoScreenState extends State<DemoScreen> with TickerProviderStateMixin {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat screen'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+
+           const Spacer(),
+            Center(
+              child:Container(
+                padding: EdgeInsets.all(20),
+                color: Colors.grey,
+                child: const NativeLabel(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 👍👍👍👍 👍👍👍👍 ',
+                ),
+              ),
+            ),
+          Center(
+               child:ElevatedButton(
+                  child: Text('Rotate'),
+                  onPressed: () {
+                    FocusScope.of(context).unfocus();
+                       Navigator.of(context).push(
+                       CubePageRoute(
+                        enterPage: DemoPage(),
+                        exitPage: context.widget,
+                        duration: const Duration(milliseconds: 500),
+                       ),
+                      );
+                  },
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+extension StringUtils on String {
+  double get va {
+    final values = split('@');
+    final num psValue =
+        int.tryParse(values.first) ?? double.tryParse(values.first) ?? 0;
+    final num fontSize =
+        int.tryParse(values.last) ?? double.tryParse(values.last) ?? 0;
+    return psValue * fontSize / 1000;
+  }
+}
