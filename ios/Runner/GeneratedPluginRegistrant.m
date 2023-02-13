e11415bb307386bd6ba988a75e36fdf58887e874
delfme <53510751+delfme@users.noreply.github.com>
2023-02-13 10:21:27 +0100

    Initial Commit

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,28 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_native_label/NativeLabelPlugin.h>)
+#import <flutter_native_label/NativeLabelPlugin.h>
+#else
+@import flutter_native_label;
+#endif
+
+#if __has_include(<flutter_native_text_input/NativeTextInputPlugin.h>)
+#import <flutter_native_text_input/NativeTextInputPlugin.h>
+#else
+@import flutter_native_text_input;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [NativeLabelPlugin registerWithRegistrar:[registry registrarForPlugin:@"NativeLabelPlugin"]];
+  [NativeTextInputPlugin registerWithRegistrar:[registry registrarForPlugin:@"NativeTextInputPlugin"]];
+}
+
+@end
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_native_text_input_example/demo_screen.dart';
+import 'package:flutter/services.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(platform: TargetPlatform.android),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Platform View Loading issue'),
+      ),
+      body:  Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (_) => const DemoScreen()));
+                },
+                child: const Text('Demo screen')),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
+  }
+}
